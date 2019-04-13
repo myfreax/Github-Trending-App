29533465301c52443de03e3a783c04fd3e538a5a
Freax <huangyanxiong2013@gmail.com>
2019-04-13 12:20:30 +0800

    Add Serializing JSON using code generation libraries step

--- travis_script.sh
+++ travis_script.sh
@@ -1,6 +1,14 @@
 set -e
+
+# install dependencies
+./flutter/bin/flutter packages get
+
+#Serializing JSON using code generation libraries
+./flutter/bin/flutter packages pub run build_runner build
+
+
 # Run the analyzer to find any static analysis issues.
-./flutter/bin/flutter analyze
+./flutter/bin/flutter analyze --no-pub
 
 # Run the formatter on all the dart files to make sure everything's linted.
 find . -not -path "./flutter/*" | grep "\.dart$" | xargs ./flutter/bin/flutter format -n
