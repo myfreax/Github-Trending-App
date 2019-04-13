ab7c328834e370f75f52ae1b7064598283ead0d4
Freax <huangyanxiong2013@gmail.com>
2019-04-13 12:48:06 +0800

    Fix remove useless code

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,7 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
-import 'package:flutter/material.dart';
+// import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:GTA/main.dart';
@@ -14,7 +14,6 @@ void main() {
   testWidgets('ListView scroll test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
-
     // Verify that our counter starts at 0.
     // expect(find.text('0'), findsOneWidget);
     // expect(find.text('1'), findsNothing);
