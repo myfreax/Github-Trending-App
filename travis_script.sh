326043383f00676f489dd02aedc988a412f8485a
Freax <huangyanxiong2013@gmail.com>
2019-04-13 11:19:08 +0800

    Add travis CI

---
+++ travis_script.sh
@@ -0,0 +1,10 @@
+set -e
+# Run the analyzer to find any static analysis issues.
+./flutter/bin/flutter analyze
+
+# Run the formatter on all the dart files to make sure everything's linted.
+find . | grep "\.dart$" | xargs ./flutter/bin/flutter format -n
+
+# Run the actual tests.
+./flutter/bin/flutter test
+echo "-- Success --"
