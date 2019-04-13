11fab1d60ff9edb08772a94cb05178bfd72abc3a
Freax <huangyanxiong2013@gmail.com>
2019-04-13 11:47:12 +0800

    Fix exclude flutter sdk dir

--- travis_script.sh
+++ travis_script.sh
@@ -3,7 +3,7 @@ set -e
 ./flutter/bin/flutter analyze
 
 # Run the formatter on all the dart files to make sure everything's linted.
-find . | grep "\.dart$" | xargs ./flutter/bin/flutter format -n
+find . -not -path "./flutter/*" | grep "\.dart$" | xargs ./flutter/bin/flutter format -n
 
 # Run the actual tests.
 ./flutter/bin/flutter test
