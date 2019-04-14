602c9e2656105102d0ef036b92ffa5b20ff588cd
Freax <huangyanxiong2013@gmail.com>
2019-04-14 18:08:48 +0800

    Add drawer and enddrawer

--- lib/main.dart
+++ lib/main.dart
@@ -2,86 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'repos.dart';
-import 'repo.dart';
+import 'views/repo.dart';
 
 const String URL =
     'https://api.github.com/search/repositories?sort=starts&order=desc&q=created:2019-04-03';
 
-final double iconSize = 12;
-
 void main(List<String> args) {
-  runApp(MyApp(repos: fetchRepos()));
-}
-
-Widget buildRepo(Repo repo) {
-  return Container(
-    padding: EdgeInsets.all(10),
-    child: Column(
-      children: <Widget>[
-        Row(children: <Widget>[
-          Expanded(
-              child: Text(
-            repo.name,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          )),
-        ]),
-        Padding(
-            child: Row(children: <Widget>[
-              Expanded(
-                  child: Text(
-                repo.description,
-                style: TextStyle(
-                  fontSize: 14,
-                ),
-              )),
-            ]),
-            padding: EdgeInsets.only(top: 5, bottom: 5)),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(Icons.language, size: iconSize),
-                  Text(repo.language)
-                ],
-              ),
-            ]),
-            Column(children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.star,
-                    size: iconSize,
-                  ),
-                  Text(repo.stars.toString())
-                ],
-              ),
-            ]),
-            Column(children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.call_split,
-                    size: iconSize,
-                  ),
-                  Text(repo.forks.toString())
-                ],
-              ),
-            ]),
-            // Column(children: <Widget>[
-            //   Row(
-            //     children: <Widget>[
-            //       Icon(Icons.star, size: iconSize),
-            //       Text(repo.todayStars.toString() + ' stars today')
-            //     ],
-            //   ),
-            // ]),
-          ],
-        )
-      ],
-    ),
-  );
+  runApp(MyApp());
 }
 
 Future<Repos> fetchRepos() async {
@@ -93,22 +20,68 @@ Future<Repos> fetchRepos() async {
   }
 }
 
-
-class MyApp extends StatelessWidget {
-  final Future<Repos> repos;
-  MyApp({Key key, this.repos}) : super(key: key);
-
+class MyappState extends State {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        endDrawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                title: Text('All'),
+                onTap: () {
+                  // change app state...
+                  // StepState();
+                  Navigator.pop(context); // close the drawer
+                },
+              ),
+              Divider(),
+              ListTile(
+                title: Text('Rust'),
+                onTap: () {
+                  // change app state...
+                  // StepState();
+                  Navigator.pop(context); // close the drawer
+                },
+              ),
+            ],
+          ),
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: <Widget>[
+              AboutListTile(
+                child: Text('About'),
+                icon: Icon(Icons.help),
+                applicationIcon: Icon(Icons.trending_up),
+                applicationName: 'GTA',
+                applicationVersion: '0.1.0',
+                applicationLegalese: 'Github Trending app built with Flutter',
+              )
+            ],
+          ),
+        ),
         appBar: AppBar(
           title: Text('Github trending'),
+          actions: <Widget>[
+            Builder(builder: (context){
+              return Padding(
+                child: IconButton(
+                  icon: Icon(Icons.filter_list),
+                  onPressed: () {
+                    Scaffold.of(context).openEndDrawer();
+                  },
+                ),
+                padding: EdgeInsets.only(right: 0));
+            },),
+            
+          ],
         ),
         body: FutureBuilder(
-          future: repos, 
-          builder: (context, snapshot) {
-            if (snapshot.hasData) {
+          future: fetchRepos(),
+          builder: (BuildContext context,AsyncSnapshot snapshot){
+          if (snapshot.hasData) {
               List<Widget> list = [];
               for (var i = 0; i < snapshot.data.items.length; i++) {
                 list.add(buildRepo(snapshot.data.items[i]));
@@ -122,9 +95,16 @@ class MyApp extends StatelessWidget {
             }
             // By default, show a loading spinner
             return Center(child: CircularProgressIndicator());
-          },
-        ),
+        },),
       ),
     );
+  }
+}
+
+class MyApp extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyappState();
   }
 }
