c0f1c69a1a0efdabf4aa4211f9ae0e616752ba54
Freax <huangyanxiong2013@gmail.com>
2019-05-28 14:18:37 +0800

    Add click event for repo

--- lib/views/repo.dart
+++ lib/views/repo.dart
@@ -3,72 +3,76 @@ import '../repo.dart';
 
 Widget buildRepo(Repo repo) {
   final double iconSize = 12;
-  return Container(
-    padding: EdgeInsets.all(10),
-    child: Column(
-      children: <Widget>[
-        Row(children: <Widget>[
-          Expanded(
-              child: Text(
-            repo.name,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-          )),
-        ]),
-        Padding(
-            child: Row(children: <Widget>[
+  return GestureDetector(
+      onTap: () {
+        print('click');
+      },
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            Row(children: <Widget>[
               Expanded(
                   child: Text(
-                repo.description,
-                style: TextStyle(
-                  fontSize: 14,
-                ),
+                repo.name,
+                style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               )),
             ]),
-            padding: EdgeInsets.only(top: 5, bottom: 5)),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: <Widget>[
-            Column(children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(Icons.language, size: iconSize),
-                  Text(' ' + repo.language)
-                ],
-              ),
-            ]),
-            Column(children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.star,
-                    size: iconSize,
+            Padding(
+                child: Row(children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    repo.description,
+                    style: TextStyle(
+                      fontSize: 14,
+                    ),
+                  )),
+                ]),
+                padding: EdgeInsets.only(top: 5, bottom: 5)),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Column(children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.language, size: iconSize),
+                      Text(' ' + repo.language)
+                    ],
                   ),
-                  Text(' ' + repo.star.toString())
-                ],
-              ),
-            ]),
-            Column(children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(
-                    Icons.call_split,
-                    size: iconSize,
+                ]),
+                Column(children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.star,
+                        size: iconSize,
+                      ),
+                      Text(' ' + repo.star.toString())
+                    ],
                   ),
-                  Text(' '+repo.fork.toString())
-                ],
-              ),
-            ]),
-            Column(children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Icon(Icons.star, size: iconSize),
-                  Text(' ' + repo.todayStar.toString())
-                ],
-              ),
-            ]),
+                ]),
+                Column(children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(
+                        Icons.call_split,
+                        size: iconSize,
+                      ),
+                      Text(' ' + repo.fork.toString())
+                    ],
+                  ),
+                ]),
+                Column(children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.star, size: iconSize),
+                      Text(' ' + repo.todayStar.toString())
+                    ],
+                  ),
+                ]),
+              ],
+            )
           ],
-        )
-      ],
-    ),
-  );
+        ),
+      ));
 }
