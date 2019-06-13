5edb003e45369af5cc18c806995b52dfb22f5eeb
Freax <huangyanxiong2013@gmail.com>
2019-06-13 21:23:13 +0800

    Remove named route

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
 import 'package:GTA/models/repos.dart';
 import 'package:GTA/views/lists.dart';
-import 'package:GTA/views/detail.dart';
 
 void main(List<String> args) async {
   ReposModel reposModel = ReposModel();
@@ -28,8 +27,6 @@ class MyappState extends State {
       routes: {
         // 当导航到/路由时就会构建ListsWidget
         '/': (context) => ListsWidget(),
-        // 当导航到/details路由时就会构建RepoDetail
-        '/detail': (context) => RepoDetail(),
       },
     );
   }
